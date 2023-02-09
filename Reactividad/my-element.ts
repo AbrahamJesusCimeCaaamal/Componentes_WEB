5415f782c9205e9dc7f7de721d17e2f547ea9afe
Abraham Jesus Cime Caamal <abraham.19070019@itsmotul.edu.mx>
2023-02-09 05:40:50 -0600

    Paso 5.- Cálculo de valores durante una actualización

--- Reactividad/my-element.ts
+++ Reactividad/my-element.ts
@@ -1,55 +1,42 @@
-import {LitElement, html, css, PropertyValues} from 'lit';
-import {customElement, property, state} from 'lit/decorators.js';
-import {classMap} from 'lit/directives/class-map.js';
-import {animate} from '@lit-labs/motion';
+import {LitElement, html, PropertyValues} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
 
 @customElement('my-element')
 export class MyElement extends LitElement {
-  @property({type: Boolean}) big = false;
-  @property({type: Number}) duration = 500;
-  @state() _renderCount = 0;
+  @property() forward = '';
+  @property() backward = '';
 
-  static styles = css`
-    .bar {
-      background: red;
-      height: 2em;
-      width: 10vw;
+  //willUpdate() devolución de llamada es el lugar 
+  //ideal para hacer estos cálculos en Lit. Se llama 
+  //cerca del comienzo del ciclo de actualización, antes de que se represente el componente
+
+
+  //La willUpdate() devolución de llamada recibe un mapa de propiedades cambiadas como argumento.
+  willUpdate(changedProperties: PropertyValues<this>) {
+    if (changedProperties.has('forward')) {
+      this.backward = this.forward.split('').reverse().join('');
     }
 
-    .big {
-      width: 50vw;
+    if (changedProperties.has('backward')) {
+      this.forward = this.backward.split('').reverse().join('');
     }
-  `;
+  }
 
-  setDuration(e: Event) {
-    const v = (e.target as HTMLSelectElement).value;
-    this.duration = Number.parseInt(v);
+  onInput(e: Event) {
+    const inputEl = e.target as HTMLInputElement;
+    if (inputEl.id === 'forward') {
+      this.forward = inputEl.value;
+    } else {
+      this.backward = inputEl.value;
+    }
   }
-//requestUpdate() desencadena un ciclo de actualización reactiva.
-// Al comienzo de un ciclo de actualización,
-// Lit llama al shouldUpdate()método, pasando un mapa de propiedades modificadas.
-  shouldUpdate(changedProperties: PropertyValues<this>): boolean {
-    return !(changedProperties.size === 1 && changedProperties.has('duration'));
-  }
-  //shouldUpdate()siempre devuelve true. Puede anular shouldUpdate()
-  //para determinar si el componente necesita actualizarse
 
   render() {
-    this._renderCount++;
-    const keyframeOptions = { duration: this.duration };
-
     return html`
-      <p>
-        <button @click=${() => (this.big = !this.big)}>Animate</button>
-      </p>
-      <p>
-        <label>Speed <select @change=${this.setDuration}>
-          <option value="250" selected>Fast</option>
-          <option value="1500">Slow</option>
-        </select></label>
-        Render count: ${this._renderCount}
-      </p>
-      <p class="bar ${classMap({big: this.big})}" ${animate({keyframeOptions})}></p>
+      <label>Forward: <input id="forward" @input=${this.onInput} .value=${this.forward}></label>
+      <label>Backward: <input id="backward" @input=${this.onInput} .value=${this.backward}></label>
+      <div>Forward text: ${this.forward}</div>
+      <div>Backward text: ${this.backward}</div>
     `;
   }
 }
