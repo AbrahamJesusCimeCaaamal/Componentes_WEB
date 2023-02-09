752787a45a464378485147740f3db77688250fde
Abraham Jesus Cime Caamal <abraham.19070019@itsmotul.edu.mx>
2023-02-09 06:33:42 -0600

    Paso 2.- Crear un elemento de patrón

--- Patronaje con SVG/repeat-patterns.ts
+++ Patronaje con SVG/repeat-patterns.ts
@@ -1,131 +1,34 @@
-import type {SVGTemplateResult} from "lit";
 
-import {LitElement, html, svg, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
 
-//SVG es un estándar web que permite a los artistas y diseñadores
-// crear hermosos gráficos con sintaxis XML.
+//El elemento en esta demostración será una sola pieza de texto. Podemos crear texto en SVG y Lit con plantillas SVG 
 
-//Lit puede componer SVG con plantillas creando gráficos portátiles y expresivos para la web.
+import type { SVGTemplateResult } from "lit";
 
-//Lit (componentes web) y SVG son compatibles con todos los navegadores modernos.
+import { LitElement, html, svg } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
 
-const themeCSS = css`
-  .background {
-    fill: var(--background-color, #000000);
-  }
-
-  text {
-    fill: var(--font-color, #ffffff);
-    font-size: var(--font-size, 26px);
-    stroke-width: var(--stroke-width, 1.2px);
-    stroke: var(--stroke-color, #eeeeee);
-  }
-`;
-
-const svgCSS = css`
-  :host {
-    display: block;
-  }
-
-  svg {
-    height: 100%;
-    width: 100%;
-  }
-
-  text {
-    fill: #ffffff;
-    dominant-baseline: hanging;
-    font-family: monospace;
-    font-size: 24px;
-  }
-`;
-
+//createElementque genere un <text>elemento con texto de visualización configurable usando una 
+//plantilla Lit SVG. Agregue atributos de estilo específicos de texto al <text>elemento.
 const createElement = (chars: string): SVGTemplateResult => svg`
-  <text id="chars">${chars}</text>
-`;
-
-const createMotif = (
-  numPrints: number,
-  offset: number = 0,
-): SVGTemplateResult => {
-  const rotation = 360 / numPrints;
-
-  const prints = [];
-  let currRotation = offset;
-  for (let index = 0; index < numPrints; index++) {
-    currRotation += rotation;
-    prints.push(svg`
-      <use
-        href="#chars"
-        transform="rotate(${currRotation}, 0, 0)">
-      </use>
-    `);
-  }
-
-  return svg`
-    <g
-      id="motif"
-      transform="translate(50, 50)">
-        ${prints}
-    </g>
-  `;
-};
-
-const createTileBoundary = () => svg`
-  <clipPath id="rect-clip">
-    <rect width="200" height="200"></rect>
-  </clipPath>
-`;
-
-const createTile = () => svg`
-  <g clip-path="url(#rect-clip)">
-    <use transform="translate(0, 0)" href="#motif"></use>
-    <use transform="translate(0, 100)" href="#motif"></use>
-    <use transform="translate(100, -50)" href="#motif"></use>
-    <use transform="translate(100, 50)" href="#motif"></use>
-    <use transform="translate(100, 150)" href="#motif"></use>
-  </g>
-`;
-
-const createRepeatPattern = () => svg`
-  <pattern
-    id="repeat-pattern"
-    x="-10"
-    y="-10"
-    width="200"
-    height="200"
-    patternUnits="userSpaceOnUse">
-    ${createTile()}
-  </pattern>
+  <text
+    dominant-baseline="hanging"
+    font-family="monospace"
+    font-size="24px">
+    ${chars}
+  </text>
 `;
 
 @customElement('repeat-pattern')
-export class RepeatPattern extends LitElement {
-  static styles = [svgCSS, themeCSS];
-
+export class RepeatPattern extends LitElement {    
   @property({type: String}) chars = "lit";
-  @property({type: Number, attribute: "num-prints"}) numPrints = 7;
-  @property({
-    type: Number,
-    attribute: "rotation-offset",
-  }) rotationOffset = 0;
-
+  
+  //una plantilla SVG debe ser hija de un <svg>elemento. 
+  //El <svg> elemento debe estar contenido dentro de una plantilla HTML
   render() {
+    //createElement la render() función de un repeat-pattern componente.
     return html`
-      <svg>
-        <defs>
-          ${createTileBoundary()}
-          ${createElement(this.chars)}
-          ${createMotif(
-            this.numPrints,
-            this.rotationOffset,
-          )}
-          ${createRepeatPattern()}
-        </defs>
-
-        <rect class="background" height="100%" width="100%"></rect>
-        <rect fill="url(#repeat-pattern)" height="100%" width="100%"></rect>
+      <svg height="100%" width="100%">
+        ${createElement(this.chars)}
       </svg>
     `;
   }
