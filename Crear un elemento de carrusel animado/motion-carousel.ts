635d3d25d470741cc5bfc98e9ee37e8332d300a5
Abraham Jesus Cime Caamal <abraham.19070019@itsmotul.edu.mx>
2023-02-09 08:25:12 -0600

    Paso 3.- Crear la propiedad seleccionada

--- Crear un elemento de carrusel animado/motion-carousel.ts
+++ Crear un elemento de carrusel animado/motion-carousel.ts
@@ -1,13 +1,29 @@
-import {LitElement, html} from 'lit';
+import {LitElement, html, PropertyValues} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {styles} from './styles.js';
-//Eso se hace con overflowde hidden. Los elementos tienen
-// un tamaño explícito 100%para que quepan dentro del carrusel.
+
 @customElement('motion-carousel')
 export class MotionCarousel extends LitElement {
   static styles = styles;
 
+  private selectedInternal = 0;
+  @property({type: Number})
+  selected = 0;
+//El carrusel ahora muestra su primer elemento secundario, pero no hay forma
+// de cambiar lo que se muestra. Necesita una selectedpropiedad para controlar qué elemento se muestra.
+  get maxSelected() {
+    return this.childElementCount - 1;
+  }
+
+  hasValidSelected() {
+    return this.selected >= 0 && this.selected <= this.maxSelected;
+  }
+//selected cualquier valor, pero solo es una selección 
+//válida si es un número en el rango del número total de elementos en el carrusel
   render() {
+    if (this.hasValidSelected()) {
+      this.selectedInternal = this.selected;
+    }
     return html`
       <div class="fit">
         <slot></slot>
