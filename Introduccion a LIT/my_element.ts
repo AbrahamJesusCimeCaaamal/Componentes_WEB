dee7d30e0014e009bdd1a10711b3c96ba56db5d4
Abraham Jesus Cime Caamal <abraham.19070019@itsmotul.edu.mx>
2023-02-08 23:54:20 -0600

    Paso 1.- Introducción a lit

---
+++ Introduccion a LIT/my_element.ts
@@ -0,0 +1,18 @@
+// Primer paso del tutorial 
+
+//Manejo de elementos con typescrip
+import {LitElement, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+  @property()
+  version = 'COMPLETED';
+
+  render() {
+    return html`
+    <p>Welcome to the Lit tutorial!</p>
+    <p>This is the ${this.version} code.</p>
+    `;
+  }
+}
