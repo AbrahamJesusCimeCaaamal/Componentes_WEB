a07cca104a9bf6158c4575010a909f24048b623d
Abraham Jesus Cime Caamal <abraham.19070019@itsmotul.edu.mx>
2023-02-09 09:23:41 -0600

    Paso 4.- ¡Una sola palabra a la vez!

--- ¡Construye un visor de palabras!/word-viewer.ts
+++ ¡Construye un visor de palabras!/word-viewer.ts
@@ -8,10 +8,16 @@ import { customElement } from 'lit/decorators.js';
 //propiedades reactivas de Lit son propiedades especiales
 // que pueden interactuar con el ciclo de vida de renderizado de Lit.
 class WordViewer extends LitElement {
+  @state() private idx = 0;
   @property() words = 'initial value';
 //el componente programa una actualización. De forma predeterminada, Lit configura la sincronización de
 // atributos para la propiedad y actualizará la propiedad cuando cambie el atributo.
   render() {
-    return html`<pre>${this.words}</pre>`
+    //El estado reactivo interno se refiere a las propiedades
+    // reactivas que no forman parte de la API pública del componente.
+    const splitWords = this.words.split('.');
+    //% operador mantendrá el índice resultante dentro de los límites de la splitWordsmatriz.
+    const word = splitWords[this.idx % splitWords.length];
+    return html`<pre>${word}</pre>`;
   }
 }
