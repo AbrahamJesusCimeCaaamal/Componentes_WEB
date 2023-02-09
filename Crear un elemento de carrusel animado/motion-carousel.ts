8bc5acb38f534dfc6b95736a7c3bd4909cf78e70
Abraham Jesus Cime Caamal <abraham.19070019@itsmotul.edu.mx>
2023-02-09 08:39:26 -0600

    Paso 5.-Cambiar el elemento seleccionado

--- Crear un elemento de carrusel animado/motion-carousel.ts
+++ Crear un elemento de carrusel animado/motion-carousel.ts
@@ -17,23 +17,30 @@ export class MotionCarousel extends LitElement {
   hasValidSelected() {
     return this.selected >= 0 && this.selected <= this.maxSelected;
   }
-  // elementos slotpara gestionar la visualización del elemento seleccionado.
 
   render() {
+    //render método para agregar la @click
     if (this.hasValidSelected()) {
       this.selectedInternal = this.selected;
     }
-    // name atributo de slotpara selectedque solo se muestre el elemento seleccionado.
+    //La @click expresión en la plantilla llama al clickHandler método cuando el usuario hace clic en el carrusel.
     return html`
-      <div class="fit">
+      <div class="fit" @click=${this.clickHandler}>
         <slot name="selected"></slot>
       </div>
     `;
   }
+//shiftKey para permitir que el usuario retroceda. 
+//El valor se ajusta al primer o último elemento cuando es necesario.
+  private clickHandler(e: MouseEvent) {
+    const i = this.selected + (Number(!e.shiftKey) || -1);
+    this.selected = i > this.maxSelected ? 0 : i < 0 ? this.maxSelected : i;
+    const change = new CustomEvent('change',
+      {detail: this.selected, bubbles: true, composed: true});
+    this.dispatchEvent(change);
+  }
 
   private previous = 0;
-  //El updatedmétodo primero verifica si necesita hacer algún trabajo, 
-  //usando el changedPropertiesargumento y el resultado de hasValidSelected().
   protected updated(changedProperties: PropertyValues) {
     if (changedProperties.has('selected') && this.hasValidSelected()) {
       this.updateSlots();
