cc23fd616877bde4e7e7b73ecc3b34d38c716214
Abraham Jesus Cime Caamal <abraham.19070019@itsmotul.edu.mx>
2023-02-09 01:15:38 -0600

    Paso 7.- Manejo de eventos en listas

--- Trabajar con listas/my-element.ts
+++ Trabajar con listas/my-element.ts
@@ -1,44 +1,37 @@
 import {LitElement, html} from 'lit';
 import {customElement, state} from 'lit/decorators.js';
-import {repeat} from 'lit/directives/repeat.js';
+import {map} from 'lit/directives/map.js';
 
 @customElement('my-element')
 class MyElement extends LitElement {
   @state()
-  tasks = [
-    { id: 'a', label: 'Learn Lit'},
-    { id: 'b', label: 'Feed the cat'},
-    { id: 'c', label: 'Go for a walk'},
-    { id: 'd', label: 'Take a nap'},
+  things = [
+    "Raindrops on roses",
+    "Whiskers on kittens",
+    "Bright copper kettles",
+    "Warm woolen mittens",
   ];
-  //repeat()en lugar de la map()directiva, proporcionando iterable, una función clave que devuelve 
-  //un identificador único para un elemento en particular y la plantilla para representar cada elemento.
 
   render() {
     return html`
-      <p>Things to do today:</p>
-      <button @click=${() => this._sort(1)}>Sort ascending</button>
-      <button @click=${() => this._sort(-1)}>Sort descending</button>
+      <p>A few of my favorite things</p>
       <ul>
-      
-        ${repeat(
-          this.tasks,
-          (task) => task.id,
-          (task) => html`
+        ${map(
+          this.things,
+          (thing, index) => html`
             <li>
-              <label><input type="checkbox" />${task.id}) ${task.label}</label>
+              ${thing}
+              <button @click=${() => this._deleteThing(index)}>Delete</button>
             </li>
           `
         )}
       </ul>
     `;
   }
-  // la repeat()directiva para mantener el elemento de la lista y la casilla de 
-  //verificación vinculados al elemento específico de la matriz. El uso de esta 
-  //directiva con una función clave le permite a Lit mantener la asociación de clave a
-  // DOM entre las actualizaciones al mover los nodos DOM cuando sea necesario.
-  private _sort(dir: number) {
-    this.tasks.sort((a, b) => a.label.localeCompare(b.label) * dir);
-    this.requestUpdate();
+//filter()método de matriz devuelve una nueva matriz que se
+// asigna a this.things. Dado que la referencia almacenada en 
+//this.thingslos cambios, Lit sabrá actualizar el componente cuando _deleteThing()se llame.
+  private _deleteThing(index: number) {
+    this.things = this.things.filter((_, i) => i !== index);
   }
 }
