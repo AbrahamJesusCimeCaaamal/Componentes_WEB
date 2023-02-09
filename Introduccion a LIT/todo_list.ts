abe16565588070520e1d014a8bb172e8f38de40f
Abraham Jesus Cime Caamal <abraham.19070019@itsmotul.edu.mx>
2023-02-09 00:36:35 -0600

    Paso 7.- Estilos

--- Introduccion a LIT/todo_list.ts
+++ Introduccion a LIT/todo_list.ts
@@ -1,29 +1,51 @@
 import {LitElement, html} from 'lit';
 import {customElement, state, query} from 'lit/decorators.js';
 
-// la _listItems propiedad se define como estado reactivo interno . 
-//Funciona como una propiedad reactiva, pero no se expone como un atributo y 
-//no se debe acceder desde fuera del componente.
-
-@customElement('todo-list')
-export class ToDoList extends LitElement {
-  @state()
-  private _listItems = [
-    { text: 'Start Lit tutorial', completed: true },
-    { text: 'Make to-do list', completed: false }
-  ];
- 
-  render() {
-    return html`
-      <h2>To Do</h2>
-      <ul>
-        ${this._listItems.map((item) =>
-          html`<li>${item.text}</li>`)}
-      </ul>
-      <input id="newitem" aria-label="New item">
-      <button @click=${this.addToDo}>Add</button>
-    `;
+type ToDoItem = {
+    text: string,
+    completed: boolean
   }
+  
+// el código de inicio se transfiere del último paso, pero hemos agregado la 
+//capacidad de marcar un elemento como completado al hacer clic en él.
+  @customElement('todo-list')
+  export class ToDoList extends LitElement {
+    //styles Agregue un campo de clase estático
+    static styles = css`
+      .completed {
+        text-decoration-line: line-through;
+        color: #777;
+      }
+    `;
+  
+    @state()
+    private _listItems = [
+      { text: 'Make to-do list', completed: true },
+      { text: 'Add some styles', completed: true }
+    ];
+  
+    render() {
+      return html`
+        <h2>To Do</h2>
+        <ul>
+          ${this._listItems.map((item) =>
+            html`
+              <li
+                  class=${item.completed ? 'completed' : ''}
+                  @click=${() => this.toggleCompleted(item)}>
+                ${item.text}
+              </li>`
+          )}
+        </ul>
+        <input id="newitem" aria-label="New item">
+        <button @click=${this.addToDo}>Add</button>
+      `;
+    }
+  
+    toggleCompleted(item: ToDoItem) {
+      item.completed = !item.completed;
+      this.requestUpdate();
+    }
   //El @querydecorador (usado en la versión TypeScript del código) es 
  //una forma práctica de obtener una referencia a un nodo en el DOM interno de su componente.
 
