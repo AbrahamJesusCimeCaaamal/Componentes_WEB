89e1d1621225d9d82ee42827dbb1544dce9f44fe
Abraham Jesus Cime Caamal <abraham.19070019@itsmotul.edu.mx>
2023-02-09 08:42:57 -0600

    Paso 6.-¡Prepara la plantilla para la animación!

--- Crear un elemento de carrusel animado/motion-carousel.ts
+++ Crear un elemento de carrusel animado/motion-carousel.ts
@@ -1,8 +1,10 @@
 import {LitElement, html, PropertyValues} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 import {styles} from './styles.js';
-
+import {styleMap} from 'lit/directives/style-map.js';
 @customElement('motion-carousel')
+
+//un elemento de "contenedor" que rodea los elementos que se animarán a la izquierda o a la derecha.
 export class MotionCarousel extends LitElement {
   static styles = styles;
 
@@ -18,20 +20,31 @@ export class MotionCarousel extends LitElement {
     return this.selected >= 0 && this.selected <= this.maxSelected;
   }
 
+  private left = 0;
   render() {
-    //render método para agregar la @click
     if (this.hasValidSelected()) {
       this.selectedInternal = this.selected;
     }
-    //La @click expresión en la plantilla llama al clickHandler método cuando el usuario hace clic en el carrusel.
+    const animateLeft = ``;
+    const selectedLeft = ``;
+    const previousLeft = ``;
+    //El @click controlador ahora tiene más sentido en el "contenedor", 
+    //por lo que no se pierden clics
     return html`
-      <div class="fit" @click=${this.clickHandler}>
-        <slot name="selected"></slot>
+      <div class="fit"
+        @click=${this.clickHandler}
+        style=${styleMap({left: animateLeft})}
+      >
+        <div class="fit" style=${styleMap({left: previousLeft})}>
+          <slot name="previous"></slot>
+        </div>
+        <div class="fit selected" style=${styleMap({left: selectedLeft})}>
+          <slot name="selected"></slot>
+        </div>
       </div>
     `;
   }
-//shiftKey para permitir que el usuario retroceda. 
-//El valor se ajusta al primer o último elemento cuando es necesario.
+
   private clickHandler(e: MouseEvent) {
     const i = this.selected + (Number(!e.shiftKey) || -1);
     this.selected = i > this.maxSelected ? 0 : i < 0 ? this.maxSelected : i;
@@ -39,7 +52,8 @@ export class MotionCarousel extends LitElement {
       {detail: this.selected, bubbles: true, composed: true});
     this.dispatchEvent(change);
   }
-
+//El selectedelemento no se mostrará después de este cambio en la plantilla.
+// Eso es porque se mueve por debajo de la ranura anterior.
   private previous = 0;
   protected updated(changedProperties: PropertyValues) {
     if (changedProperties.has('selected') && this.hasValidSelected()) {
